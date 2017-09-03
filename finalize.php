5eb8e5b91cfd07e911eae5ad82be7d0d789562c5
Alexandre Magno <alexandre.mbm@gmail.com>
2017-09-03 08:12:56 +0200

    finalize.php

---
+++ finalize.php
@@ -0,0 +1,11 @@
+<?php
+
+$old_path = getcwd();
+chdir('/home/alexandre.mbm/pwc');
+$output = shell_exec('./run.sh');
+chdir($old_path);
+echo "<pre>\n";
+print_r(utf8_decode($output));
+echo "\n</pre>";
+
+?>
