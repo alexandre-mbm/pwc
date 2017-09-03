95227613b15a8c5fa32b1b066534bac5d7b198e5
Alexandre Magno <alexandre.mbm@gmail.com>
2017-09-03 08:27:09 +0200

    font-size

--- finalize.php
+++ finalize.php
@@ -4,7 +4,7 @@ $old_path = getcwd();
 chdir('/home/alexandre.mbm/pwc');
 $output = shell_exec('./run.sh');
 chdir($old_path);
-echo "<pre>\n";
+echo "<pre style='font-size:4em;'>\n";
 print_r(utf8_decode($output));
 echo "\n</pre>";
 
