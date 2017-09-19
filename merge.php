579754ee86249593c11ee4ca9263136ad4c67e46
Alexandre Magno <alexandre.mbm@gmail.com>
2017-09-19 18:30:52 +0200

    Quatro PNG ou PDF

--- merge.php
+++ merge.php
@@ -27,6 +27,11 @@ temporize('Aviso de férias.pdf');
 temporize('Recibo_Ferias.pdf');
 temporize('recibo.pdf');
 
+temporize('CAERN.pdf');
+temporize('COSERN.pdf');
+temporize('Comprovante.pdf');
+temporize('NET.pdf');
+
 chdir($tmp);
 
 $pdf = new PDFMerger;
@@ -43,12 +48,10 @@ try {
 }
 
 try {
-    if (!file_exists(__DIR__ . '/' . $src . '/' . 'CAERN.png'))
-	    throw new Exception('CAERN.png não existe');
+    if (!file_exists(__DIR__ . '/' . $src . '/' . 'CAERN.png') && !file_exists('CAERN.pdf'))
+	    throw new Exception('CAERN.png e CAERN.pdf não existem');
     if (!file_exists(__DIR__ . '/' . $src . '/' . 'relatorio.pdf'))
 	    throw new Exception('relatorio.pdf não existe');
-    if (!file_exists('CAERN.pdf'))
-	    throw new Exception('CAERN.pdf não foi gerado');
     $pdf->addPDF('relatorio.pdf')
         ->addPDF('CAERN.pdf');
 } catch (Exception $e) {
@@ -56,12 +59,10 @@ try {
 }
 
 try {
-    if (!file_exists(__DIR__ . '/' . $src . '/' . 'COSERN.png'))
-	    throw new Exception('COSERN.png não existe');
+    if (!file_exists(__DIR__ . '/' . $src . '/' . 'COSERN.png') && !file_exists('COSERN.pdf'))
+	    throw new Exception('COSERN.png e COSERN.pdf não existem');
     if (!file_exists(__DIR__ . '/' . $src . '/' . '2a Via de Fatura.pdf'))
 	    throw new Exception('2a Via de Fatura.pdf não existe');
-    if (!file_exists('COSERN.pdf'))
-	    throw new Exception('COSERN.pdf não foi gerado');
     $pdf->addPDF('2a Via de Fatura.pdf', '1')
         ->addPDF('COSERN.pdf');
 } catch (Exception $e) {
@@ -89,12 +90,10 @@ try {
 }
 
 try {
-    if (!file_exists(__DIR__ . '/' . $src . '/' . 'Comprovante.png'))
-	    throw new Exception('Comprovante.png não existe');
+    if (!file_exists(__DIR__ . '/' . $src . '/' . 'Comprovante.png') && !file_exists('Comprovante.pdf'))
+	    throw new Exception('Comprovante.png e Comprovante.pdf não existem');
     if (!file_exists(__DIR__ . '/' . $src . '/' . 'GuiaPagamento.PDF'))
 	    throw new Exception('GuiaPagamento.PDF não existe');
-    if (!file_exists('Comprovante.pdf'))
-	    throw new Exception('Comprovante.pdf não foi gerado');
     $pdf->addPDF('GuiaPagamento.PDF')
         ->addPDF('Comprovante.pdf');
 } catch (Exception $e) {
@@ -111,12 +110,10 @@ try {
 }
 
 try {
-    if (!file_exists(__DIR__ . '/' . $src . '/' . 'NET.png'))
-	    throw new Exception('NET.png não existe');
+    if (!file_exists(__DIR__ . '/' . $src . '/' . 'NET.png') && !file_exists('NET.pdf'))
+	    throw new Exception('NET.png e NET.pdf não existem');
     if (!file_exists(__DIR__ . '/' . $src . '/' . 'Fatura Net.pdf'))
 	    throw new Exception('Fatura Net.pdf não existe');
-    if (!file_exists('NET.pdf'))
-	    throw new Exception('NET.pdf não foi gerado');
     $pdf->addPDF('Fatura Net.pdf', '1')
         ->addPDF('NET.pdf');
 } catch (Exception $e) {
